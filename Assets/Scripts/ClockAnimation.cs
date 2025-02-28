bcef15ee4778c80a1a0feffae6bdd58da4cee74a
YoannMnts <yoann.montesinos@gmail.com>
2025-02-28 11:02:40 +0100

    Code Anim Clock

--- Assets/Scripts/ClockAnimation.cs
+++ Assets/Scripts/ClockAnimation.cs
@@ -1,10 +1,100 @@
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ClockAnimation : MonoBehaviour
 {
+    [SerializeField] private GameObject[] screwObject, interactionObject, clockButton, underClock, hatchSide, key, hatchKey, dial;
     [SerializeField] private Dialog dialog;
+    private int currentScrewDriver;
+    private int currentInteraction;
+    private bool hasKey;
     void Start()
     {
         dialog.ShowDialog();
+    }
+    
+    public void PlayAnimationScrew(GameObject screw)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if(screwObject[i] == screw)
+                screwObject[i].GetComponent<Animator>().enabled = true;
+        }
+        currentScrewDriver++;
+    }
+    public void PlayAnimationHatch()
+    {
+        if (currentScrewDriver == 4)
+        {
+            interactionObject[currentInteraction].GetComponent<Animator>().enabled = true;
+        }
+    }
+
+    public void PlayAnimationClockButton()
+    {
+        clockButton[currentInteraction].GetComponent<Animator>().enabled = true;
+        underClock[currentInteraction].SetActive(false);
+        dialog.ShowDialog();
+    }
+
+    public void PlayAnimationHatchSide()
+    {
+        hatchSide[currentInteraction].GetComponent<Animator>().enabled = true;
+    }
+
+    public void showInputField()
+    {
+        this.gameObject.SetActive(true);
+    }
+    public void codeEntry(String hour)
+    {
+        if (hasKey)
+        {
+            if (hour == "1846")
+            {
+                PlayAnimationDial();
+            }
+        }
+        else
+        {
+            if (hour == "4751")
+            {
+                PlayAnimationHatchSide();
+                dialog.ShowDialog();
+            }
+        }
+        this.gameObject.SetActive(false);
+    }
+
+    public void PlayAnimationMecanism()
+    {
+        dialog.ShowDialog();
+        key[currentInteraction].SetActive(true);
+    }
+
+    public void GetKey()
+    {
+        key[currentInteraction].SetActive(false);
+        hasKey = true;
+    }
+
+    public void PlayAnimationHatchKey()
+    {
+        hatchKey[currentInteraction].GetComponent<Animator>().enabled = true;
+        dialog.ShowDialog();
+    }
+
+    public void PlayAnimationDial()
+    {
+        dial[currentInteraction].GetComponent<Animator>().enabled = true;
+    }
+
+    public void Adress()
+    {
+        dialog.ShowDialog();
+        SceneManager.LoadScene(2);
     }
 }
--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -8,6 +8,9 @@ public class Interaction : MonoBehaviour
     [SerializeField] private Tool tool;
     public bool isScrewDriver;
     [SerializeField] private PhoneAnimation phoneAnimation;
+    [SerializeField] private ClockAnimation clockAnimation;
+    [SerializeField] private Dialog dialog;
+    private bool hasClickerOnBomb;
 
     private void Update()
     {
@@ -15,13 +18,66 @@ public class Interaction : MonoBehaviour
         {
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
             {
-                if(hit.collider.CompareTag("PhoneUnderCover") && isInteraction)
+                
+                if (hit.collider.CompareTag("ClockScrew") && isScrewDriver)
                 {
-                    phoneAnimation.PlayAnimationUnderPhone();
+                    clockAnimation.PlayAnimationScrew(hit.collider.gameObject);
                 }
 
-                if (hit.collider.CompareTag("PhoneScrew") && isScrewDriver)
+                if (hit.collider.CompareTag("ClockHatchBack") && isInteraction)
                 {
+                    clockAnimation.PlayAnimationHatch();
+                }
+
+                if (hit.collider.CompareTag("ClockButton") && isInteraction)
+                {
+                    clockAnimation.PlayAnimationClockButton();
+                }
+
+                if (hit.collider.CompareTag("Bomb") && isInteraction)
+                {
+                    if (hasClickerOnBomb == false)
+                    {
+                        dialog.ShowDialog();
+                        hasClickerOnBomb = true;
+                    }
+                    else if (hasClickerOnBomb == true)
+                    {
+                        clockAnimation.showInputField();
+                    }
+                }
+
+                if (hit.collider.CompareTag("Mecanism") && isInteraction)
+                {
+                    clockAnimation.PlayAnimationMecanism();
+                }
+
+                if (hit.collider.CompareTag("Key") && isInteraction)
+                {
+                    clockAnimation.GetKey();
+                }
+
+                if (hit.collider.CompareTag("HatchKey") && isInteraction)
+                {
+                    clockAnimation.PlayAnimationHatchKey();
+                }
+
+                if (hit.collider.CompareTag("Needle") && isInteraction)
+                {
+                    clockAnimation.showInputField();
+                }
+
+                if (hit.collider.CompareTag("Adress") && isInteraction)
+                {
+                    clockAnimation.Adress();
+                }
+                
+                if(hit.collider.CompareTag("PhoneUnderCover") && isInteraction)
+                { 
+                    phoneAnimation.PlayAnimationUnderPhone();
+                } 
+                if (hit.collider.CompareTag("PhoneScrew") && isScrewDriver)
+                { 
                     phoneAnimation.PlayAnimationScrew(hit.collider.gameObject);
                 }
             }
